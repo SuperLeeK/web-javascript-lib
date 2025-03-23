02cb4fc5ccfac497c29c0ef16528b016be2366d6
이지영 <zero@walrus.kr>
2025-03-23 22:23:33 +0900

    [useKeyPress] Add keyPress listener

---
+++ libs/useKeyPress.js
@@ -0,0 +1,18 @@
+// useKeyPress.js
+
+function useKeyPress(targetKey, callback) {
+  // 이벤트 리스너 함수
+  function handleKeyPress(event) {
+    if (event.key === targetKey) {
+      callback(event);  // 해당 키가 눌렸을 때 콜백 호출
+    }
+  }
+
+  // 'keydown' 이벤트 리스너 등록
+  document.addEventListener('keydown', handleKeyPress);
+
+  // 반환된 객체에 이벤트 리스너를 제거하는 함수 포함
+  return () => {
+    document.removeEventListener('keydown', handleKeyPress);
+  };
+}

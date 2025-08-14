8c584c42f394ea4ea374bfdc92ccedaf60ca8996
이지영 <ljzero5@gmail.com>
2025-08-14 09:52:12 +0900

    Rename runWhenReady to waitFor like puppeteer

--- libs/utils.js
+++ libs/utils.js
@@ -1,10 +1,10 @@
 
 const cssToObject = (css) => {
   const cssObj = {};
-  
+
   // 각 속성 및 값에 대해 분리
   const properties = css.split(';').filter(Boolean); // 세미콜론으로 구분하고 빈 값 필터링
-  
+
   properties.forEach(property => {
     const [key, value] = property.split(':').map(item => item.trim());
     cssObj[key] = parseFloat(value.replace('px', ''));
@@ -38,20 +38,32 @@ function objectToQueryString(obj) {
   return `?${queryString}`;
 }
 
-function runWhenReady(readySelector, callback) {
+function waitFor(condition, callback) {
+  if (condition()) {
+    callback();
+  } else {
+    setTimeout(waitFor, 100, condition, callback);
+  }
+}
+
+function waitForSeconds(seconds, callback) {
+  setTimeout(callback, seconds * 1000);
+}
+
+function waitForSelector(readySelector, callback) {
   var numAttempts = 0;
-  var tryNow = function() {
-      var elem = document.querySelector(readySelector);
-      if (elem) {
-          callback(elem);
+  var tryNow = function () {
+    var elem = document.querySelector(readySelector);
+    if (elem) {
+      callback(elem);
+    } else {
+      numAttempts++;
+      if (numAttempts >= 34) {
+        console.warn('Giving up after 34 attempts. Could not find: ' + readySelector);
       } else {
-          numAttempts++;
-          if (numAttempts >= 34) {
-              console.warn('Giving up after 34 attempts. Could not find: ' + readySelector);
-          } else {
-              setTimeout(tryNow, 250 * Math.pow(1.1, numAttempts));
-          }
+        setTimeout(tryNow, 250 * Math.pow(1.1, numAttempts));
       }
+    }
   };
   tryNow();
 }
